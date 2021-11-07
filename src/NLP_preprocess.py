f80a66c566073dfa6129d9d3daf155446d8e72e6
Johanna Seif <seif.johanna@gmail.com>
2021-11-07 23:31:50 +0100

    processing with panda df, and train/test implementation

--- src/NLP_preprocess.py
+++ src/NLP_preprocess.py
@@ -4,9 +4,10 @@ import re
 import gensim
 from gensim.utils import simple_preprocess
 import nltk
-nltk.download('stopwords')
+# nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.snowball import FrenchStemmer
+import pandas as pd
 
 class AidesDataset:
     def __init__(self, json_path):
@@ -20,57 +21,63 @@ class AidesDataset:
         print("number of aides:", len(aides))
         return aides
 
-    def aide_descriptions(self):
-        ''' Filter aides, keeping only descriptions '''
-        descriptions = []
+    def filter_features(self, useful_features):
+        ''' Filter aides, keeping the features in the list useful_features '''
+        filtered_aides = []
         for aide in self.aides:
-            aide_description = {'description' : aide['description']}
-            descriptions.append(aide_description)
-        self.aides = descriptions
+            filtered_aide = {k: aide[k] for k in useful_features}
+            filtered_aides.append(filtered_aide)
+        self.aides = filtered_aides
 
-    def clean_text_features(self):
-        ''' Remove part of the punctuation and change the case to lower case'''
+    def clean_text_features(self, features_to_clean):
+        ''' Remove part of the punctuation and change the case to lower case
+            in the features features_to_clean '''
         for aide in self.aides:
-            for feature in list(aide.keys()):
+            for feature in features_to_clean:
                 if isinstance(aide[feature], str):
                     aide[feature] = re.sub('[,\.!?:;-]|<.*?>|\n', "", aide[feature])
                     aide[feature] = aide[feature].lower()
 
-    def sent_to_words(self):
-        ''' Return a list containing tokenized lists of the features' words '''
-        all_texts_list = []
-        for aide in self.aides:
-            for feature in list(aide.keys()):
-                if isinstance(aide[feature], str):
-                    all_texts_list.append(aide[feature])
-        data_words = []
-        for text in all_texts_list:
-            data_words.append(gensim.utils.simple_preprocess(str(text)))
-        return data_words
+    def to_pandas(self):
+        ''' Transform an AidesDataset to a pandas dataframe
+            with column idexed by the dictionaries keys '''
+        return pd.DataFrame.from_records(self.aides, index = 'id')
 
-def remove_stopwords(data_words):
-    ''' Remove French stopwords for a list of list of words '''
+def sent_to_words(data, selected_features):
+    ''' From a panda dataframe, transform text in features in a
+        tokenized lists of the features' words '''
+    for feature in selected_features:
+        data[feature] = data[feature].map(gensim.utils.simple_preprocess)
+    return data
+
+def remove_stopwords(data, selected_features):
+    ''' Remove French stopwords for a  list of words in selected_features '''
     stop_words = stopwords.words('french')
-    return [[word for word in feature_words if word not in stop_words]
-                                    for feature_words in data_words]
+    for feature in selected_features:
+        data[feature] = data[feature].map(
+            lambda words_list : [word for word in words_list if word not in stop_words])
+    return data
 
-def french_stemmer(data_words):
-    ''' Apply French stemming for words in a list of list of words '''
+def french_stemmer(data, selected_features):
+    ''' Apply French stemming for a  list of words in selected_features '''
     stemmer = FrenchStemmer()
-    return [[stemmer.stem(word) for word in feature_words]
-                                    for feature_words in data_words]
+    for feature in selected_features:
+        data[feature] = data[feature].map(
+            lambda words_list : [stemmer.stem(word) for word in words_list])
+    return data
 
 def get_data_words(link):
     ''' Load, clean and format the aides. Return a list of list of words '''
     # Uncomment to dowload JSON file
     dataset = AidesDataset(link)
-    dataset.aide_descriptions()
-    dataset.clean_text_features()
+    dataset.filter_features(["id", "description"])
+    dataset.clean_text_features(["description"])
 
-    data_words = dataset.sent_to_words()
+    data = dataset.to_pandas()
+    data = sent_to_words(data, ["description"])
 
     # Remove stopwords and stem
-    data_words = remove_stopwords(data_words)
-    data_words = french_stemmer(data_words)
+    data = remove_stopwords(data, ["description"])
+    data = french_stemmer(data, ["description"])
 
-    return data_words
+    return data
--- src/topic_modeling_LDA.py
+++ src/topic_modeling_LDA.py
@@ -4,6 +4,7 @@ from pprint import pprint
 from NLP_preprocess import get_data_words
 import gensim
 import gensim.corpora as corpora
+from sklearn.model_selection import train_test_split
 
 # DEBUG
 from pdb import set_trace as bp
@@ -26,21 +27,39 @@ if __name__ == '__main__':
     #Uncomment to download the json file
     #get_all_pages()
 
-    # Create the datawords list
-    data_words = get_data_words("data/AT_aides_full.json")
+    # Create the panda dataframe indexed by "id" containing the selected features
+    processed_data = get_data_words("data/AT_aides_full.json")
+    data_train, data_test = train_test_split(processed_data, test_size=100)
+
+    # LDA Training
     # Create Dictionary
+    data_words = data_train.values.flatten()
     id2word = corpora.Dictionary(data_words)
+
     # Create Corpus
-    corpus = [id2word.doc2bow(feature_words) for feature_words in data_words]
+    train_corpus = [id2word.doc2bow(feature_words) for feature_words in data_words]
 
     # number of topics
     num_topics = 10
 
     # Build LDA model
-    lda_model = gensim.models.LdaMulticore(corpus=corpus,
+    lda_model = gensim.models.LdaMulticore(corpus=train_corpus,
                                            id2word=id2word,
                                            num_topics=num_topics)
     # Print the Keyword in the 10 topics
     pprint(lda_model.print_topics())
+
+    # Testing the model
+    test_index = data_test.index[0]
+    test_corpus = [id2word.doc2bow(data_test['description'][test_index])]
+    print("====================")
+    print("Test Results")
+    print("====================")
+
+    print("Description:")
+    print(data_test['description'][test_index])
+    print("Topic:")
+    for x in lda_model.get_document_topics(test_corpus):
+        print(x)
 
     print("done")
