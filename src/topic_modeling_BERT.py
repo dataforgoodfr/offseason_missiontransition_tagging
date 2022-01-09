d55f32c4c250a9b9e0bc0166d8b6c883c3f87f75
Johanna Seif <seif.johanna@gmail.com>
2022-01-09 16:33:46 +0100

    train/test split + stats/visualisation for bertopic

--- src/topic_modeling_BERT.py
+++ src/topic_modeling_BERT.py
@@ -1,12 +1,19 @@
 
 from aides_dataset import AidesDataset
-
 from bertopic import BERTopic
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 if __name__ == '__main__':
-    version = "v3"
-    # Load data
+    version = "v1"
+
+    # Load MT data and extract MT ids
+    MT_aides_dataset = AidesDataset("data/MT_aides.json")
+    MT_id = [aide["id"] for aide in MT_aides_dataset.aides]
+
+    # Load data and extract ids
     aides_dataset = AidesDataset("data/AT_aides_full_.json")
+    id = [aide["id"] for aide in aides_dataset.aides]
 
     # Process the data
     aides_dataset.filter_features(["name", "description"])
@@ -16,31 +23,68 @@ if __name__ == '__main__':
         no_punctuation=False, # Keep the punctation (BERT handles it)
         no_upper_case=True, # Keep the case (BERT handles it)
         no_stopwords=True)
-    docs = [f'Nom : {aide["name"]}\nDescription : {aide["description"]}' for aide in aides_dataset.aides]
-    print(docs[0])
+    docs = [f'Titre : {aide["name"]}\nDescription : {aide["description"]}' for aide in aides_dataset.aides]
+
+    # Split in train/ test
+    docs_train, docs_test, id_train, id_test = train_test_split(docs, id, test_size=0.25, random_state=1)
 
     # Learn/load BERTopic model
     topic_model = BERTopic(language='French')
-    topics, probabilities = topic_model.fit_transform(docs)
+    topic_model.fit(docs_train)
     # topic_model = BERTopic.load(f"src/model/bertopic_{version}")
 
     # Save model for future use
     topic_model.save(f"src/model/bertopic_{version}")
 
-    # Display topics found
-    print(topic_model.get_topic_info())
-
     # Display, visually, topics found
     fig = topic_model.visualize_topics()
     fig.write_html(f"plots/bert/bertopic_{version}.html")
 
-    """ Ce que j'ai fait :
-        1. Retirer les balises HTML car (i) elles ne sont pas du texte utile, et
-        (ii) elles contiennent des mots en trop ("strong", "opportunities").
-            ==> N'améliore pas les topics.
-        2. Ajouter le nom de l'aide aux documents traités par BERTopic.
-            ==> Améliore un peu les topics.
-        3. Retirer les stop words, qui semblent polluer les topics.
-        Les majuscules sont aussi retirées, pour se simplifier la vie.
-            ==> Améliore un peu les topics.
-    """
+    # Save topics in csv
+    topic_model.get_topic_info().to_csv('bertopics/train_topics.csv')
+
+    # Write the topics
+    f = open("bertopics/BERT_topics", "w")
+    f.write("===================== Model general information =====================")
+    f.write("\n\nTopic information for AT full dataset:")
+    f.write("\n" + str(topic_model.get_topic_info()))
+
+    # Stats for the topics of MT in train set
+    MT_docs_train = [doc for (doc, id) in zip(docs_train, id_train) if id in MT_id]
+    topics_train, _ = topic_model.transform(MT_docs_train)
+    MT_train_topic_info = topic_model.get_topic_info().copy()
+    MT_train_topic_info.set_index("Topic", inplace=True)
+    MT_train_topic_info["Count"] = 0
+    for topic in topics_train:
+        MT_train_topic_info.at[topic, "Count"] += 1
+    MT_train_topic_info = MT_train_topic_info[MT_train_topic_info["Count"] > 0]
+    f.write("\n\nNumber of elements per topic for MT in the train set:")
+    f.write("\n" + str(MT_train_topic_info))
+
+    # Applying the model to the test elements that are in MT
+    MT_docs_test = [doc for (doc, id) in zip(docs_test, id_test) if id in MT_id]
+    MT_id_test = [id for (doc, id) in zip(docs_test, id_test) if id in MT_id]
+    topics_test, _ = topic_model.transform(MT_docs_test)
+    MT_test_topic_info = topic_model.get_topic_info().copy()
+    MT_test_topic_info.set_index("Topic", inplace=True)
+    MT_test_topic_info["Count"] = 0
+    for topic in topics_test:
+        MT_test_topic_info.at[topic, "Count"] += 1
+    MT_test_topic_info = MT_test_topic_info[MT_test_topic_info["Count"] > 0]
+    f.write("\n\nNumber of elements per topic for MT in the test set:")
+    f.write("\n" + str(MT_test_topic_info))
+
+    f.write("\n\n===================== Examples of outputs =====================")
+
+    # Output for 5 smallest descriptions
+    description_topic = [(doc, topic, id) for doc, topic, id in zip(MT_docs_test, topics_test, MT_id_test)]
+    description_topic.sort(key = lambda x : len(x[0]))
+    for i in range(30, 35):
+        f.write(f"\n\n----- Document -----")
+        for aide in MT_aides_dataset.aides:
+            if aide["id"] == description_topic[i][2]:
+                f.write(f'\n\nTitre : {aide["name"]}\nDescription : {aide["description"]}')
+        f.write("\nTopic:")
+        f.write(MT_test_topic_info.at[description_topic[i][1], "Name"])
+
+    f.close()
