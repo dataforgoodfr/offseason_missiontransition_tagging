cd3d04788ac13c0985bf727b664908dde7113fcf
AMDonati <alice.martindonati@gmail.com>
2022-01-11 18:00:25 +0100

    correct short descr tags on zeroshot classif

--- src/zeroshot_text_classification.py
+++ src/zeroshot_text_classification.py
@@ -3,6 +3,7 @@ from src.aides_dataset import AidesDataset
 import pandas as pd
 import os
 import argparse
+import numpy as np
 
 def classify_description(classifier, description, candidate_tags):
     hypothesis_template = "Ce texte est {}."
@@ -42,7 +43,11 @@ if __name__ == '__main__':
                         help='limit number of samples for debugging.')
     parser.add_argument("-thr", type=float, default=0.5,
                         help='limit number of samples for debugging.')
+    parser.add_argument("-save_path", type=str,
+                        help='save path for models. in that case, only postprocess the results.')
     args = parser.parse_args()
+
+
     classifier = pipeline("zero-shot-classification", model='cache/xnli')
 
     # read candidate tags
@@ -57,41 +62,45 @@ if __name__ == '__main__':
     processed_data = dataset.get_unfiltered_data_words(useful_features=["description", "id", "categories"])
     data_words = processed_data.values.flatten()
 
+    if args.save_path is None:
     # zero shot text classification
-    results = dict.fromkeys(["id"] + tags)
-    for key in results.keys():
-        results[key] = []
-    num_samples = len(data_words) if args.num_samples is None else args.num_samples
-    for id, descr_words in zip(list(processed_data.index[:num_samples]), data_words[:num_samples]):
-        descr = ' '.join(descr_words)
-        result = classify_description(classifier, descr, tags)
-        update_results(results, result, id)
-    # format results
-    df_results = pd.DataFrame.from_records(results)
-    df_results.set_index(keys="id", inplace=True)
-    df_results = df_results.apply(lambda t: round(t, 3))
-    print(df_results.head())
+        results = dict.fromkeys(["id"] + tags)
+        for key in results.keys():
+            results[key] = []
+        num_samples = len(data_words) if args.num_samples is None else args.num_samples
+        for id, descr_words in zip(list(processed_data.index[:num_samples]), data_words[:num_samples]):
+            descr = ' '.join(descr_words)
+            result = classify_description(classifier, descr, tags)
+            update_results(results, result, id)
+        # format results
+        df_results = pd.DataFrame.from_records(results)
+        df_results.set_index(keys="id", inplace=True)
+        df_results = df_results.apply(lambda t: round(t, 3))
+        print(df_results.head())
 
-    # get tags per description
-    list_tags = []
-    for id in df_results.index:
-        list_tags.append(get_description_tags(df_results, id, thr=args.thr))
-    tags_per_description = dict(zip(list(df_results.index), list_tags))
-    tags_per_description = pd.DataFrame.from_records(tags_per_description, index=['tags']).T
-    tags_per_description["num_tags"] = tags_per_description["tags"].apply(lambda t: len(t.split(";")))
+        # get tags per description
+        list_tags = []
+        for id in df_results.index:
+            list_tags.append(get_description_tags(df_results, id, thr=args.thr))
+        tags_per_description = dict(zip(list(df_results.index), list_tags))
+        tags_per_description = pd.DataFrame.from_records(tags_per_description, index=['tags']).T
+        tags_per_description["num_tags"] = tags_per_description["tags"].apply(lambda t: len(t.split(";")))
 
-    # save results on csv files
-    out_path = 'output/zeroshot_classif'
-    if not os.path.isdir(out_path):
-        os.makedirs(out_path)
-    df_results.to_csv(os.path.join(out_path, "results.csv"))
-    tags_per_description.to_csv(os.path.join(out_path, "tags_per_descr_thr{}.csv".format(args.thr)))
+        # save results on csv files
+        out_path = 'output/zeroshot_classif'
+        if not os.path.isdir(out_path):
+            os.makedirs(out_path)
+        df_results.to_csv(os.path.join(out_path, "results.csv"))
+        tags_per_description.to_csv(os.path.join(out_path, "tags_per_descr_thr{}.csv".format(args.thr)))
 
+    else:
+        tags_per_description = pd.read_csv(os.path.join(args.save_path, "tags_per_descr_thr{}.csv".format(args.thr)))
+        tags_per_description.set_index('Unnamed: 0', inplace=True)
     # look at short descriptions
     short_descr = dataset.get_short_descriptions(processed_data)
     short_descr["description"] = short_descr["description"].apply(lambda t: ' '.join(t))
-    short_descr["tags"] = tags_per_description.loc[list(short_descr.index)]["tags"]
-    short_descr.to_csv(os.path.join(out_path, "short_descr_tags_thr{}.csv".format(args.thr)))
+    short_descr_results = tags_per_description.merge(short_descr, left_index=True, right_index=True, how='inner')
+    short_descr_results.to_csv(os.path.join(args.save_path, "short_descr_tags_thr{}.csv".format(args.thr)))
 
     print("done")
 
